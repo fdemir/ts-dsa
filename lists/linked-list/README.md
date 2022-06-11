78e41b496f7e93d3974ce5f7abe79463d584fa0c
fdemir <frndemir@gmail.com>
2022-06-11 12:45:26 +0300

    :truck: group linked lists

---
+++ lists/linked-list/README.md
@@ -0,0 +1,6 @@
+## Linked List
+
+Linked lists can be thought of from a high level perspective as being a series of nodes. Each node has at least a single pointer to the next node, and in the last node’s case a null pointer representing that there are no more nodes in the linked list.
+
+Singly linked list: a list where elements have only a reference to next element
+Doubly linked list: a list where elements are linked to both next and previous elements
--- lists/linked-list/singly-linked-list.ts
+++ lists/linked-list/singly-linked-list.ts
@@ -1,23 +1,3 @@
-/**
- * Linked lists can be thought of from a high level perspective as being a series of nodes.
- * Each node has at least a single pointer to the next node, and in the
- * last node’s case a null pointer representing that there are no more nodes in the
- * linked list.
- */
-
-//  1. Insertion is O(1)
-//  2. Deletion is O(n)
-//  3. Searching is O(n)
-
-// 1. the list is dynamically resized, thus it incurs no copy penalty like an array
-// or vector would eventually incur; and
-// 2. insertion is O(1).
-
-// Singly linked list: a list where elements have only a reference to next element
-// Doubly linked list: a list where elements are linked to both next and previous elements
-
-// - Singly Linked List -
-
 interface ILinkedList<T> {
   add(data: T): ListNode<T>;
   delete(node: ListNode<T>): void;
