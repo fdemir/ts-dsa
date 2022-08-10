fc628097bb66137e5caed761109ef07116528b1d
fdemir <frndemir@gmail.com>
2022-08-11 00:29:28 +0300

    :sparkles: add hash table implementation

---
+++ src/hash-table/hash-table.ts
@@ -0,0 +1,75 @@
+class HashTable<T> {
+  private readonly LOAD_FACTOR = 0.75;
+
+  table: [string, T][][] = Array(3);
+  numItems = 0;
+
+  // TODO: move it to the seperate place and implement a better hash function, prevent collision
+  private hash(key: string, tableLength: number): number {
+    // ???
+    let hash = 17;
+
+    for (let i = 0; i < key.length; i++) {
+      hash = (13 * hash * key.charCodeAt(i)) % tableLength;
+    }
+    return hash;
+  }
+
+  private resize() {
+    const newTable = Array(this.table.length * 2);
+    this.table.forEach((bucket) => {
+      if (bucket) {
+        bucket.forEach(([key, value]) => {
+          this.addItem(key, value, this.table);
+        });
+      }
+    });
+
+    this.table = newTable;
+  }
+
+  /**
+   * Common logic for adding an item to the table.
+   */
+  private addItem(key: string, value: T, targetTable: [string, T][][]) {
+    const id = this.hash(key, targetTable.length);
+
+    if (targetTable[id]) {
+      targetTable[id].push([key, value]);
+    } else {
+      targetTable[id] = [[key, value]];
+    }
+  }
+
+  setItem(key: string, value: T) {
+    const loadFactor = this.numItems / this.table.length;
+
+    if (loadFactor > this.LOAD_FACTOR) {
+      this.resize();
+    }
+
+    const id = this.hash(key, this.table.length);
+
+    this.addItem(key, value, this.table);
+
+    this.numItems++;
+  }
+
+  getItem(key: string): T | undefined {
+    const id = this.hash(key, this.table.length);
+
+    if (!this.table[id]) {
+      return undefined;
+    }
+
+    // TODO: iterate through the array and return the value if found, more performant
+    return this.table[id].find(([k]) => k === key)?.[1];
+  }
+
+  removeItem() {
+    // TODO: implement
+    throw new Error("Not implemented");
+  }
+}
+
+export { HashTable };
