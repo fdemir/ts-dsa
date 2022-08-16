aca58d60c0cc50044e940dd22db3e8b965c04aab
fdemir <frndemir@gmail.com>
2022-08-16 21:50:34 +0300

    :poop: add hash table example

---
+++ src/hash-table/hash-table.example.ts
@@ -0,0 +1,22 @@
+import { HashTable } from "./hash-table";
+type CoffeeTypes = "Arabica" | "Robusta" | "Excelsa" | "Liberica";
+type Size = "sm" | "lg" | "xl";
+
+interface Shop {
+  name: string;
+  location: [number, number];
+  size: Size;
+}
+
+interface CoffeeShop extends Shop {
+  types: CoffeeTypes[];
+}
+
+const myFavoriteCofeeShops = new HashTable<CoffeeShop>();
+
+myFavoriteCofeeShops.setItem("starbucks", {
+  name: "Starbucks",
+  location: [37.78825, -122.4324],
+  size: "sm",
+  types: ["Arabica", "Robusta", "Excelsa", "Liberica"],
+});
